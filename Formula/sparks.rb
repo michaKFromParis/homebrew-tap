3ee44be09e3a5120816d46a8f9433f5f56e1e80f
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-22 16:48:40 +0100

    Brew formula update for sparks version v0.0.42

--- Formula/sparks.rb
+++ Formula/sparks.rb
@@ -1,9 +1,9 @@
 class Sparks < Formula
   desc "Design, Build & Play Sparks applications"
   homepage "https://github.com/michaKFromParis/sparks"
-  url "https://github.com/michaKFromParis/sparks/releases/download/v0.0.24/sparks-osx.tar.gz"
-  version "0.0.24"
-  sha256 "7b50e0320826964620d49bb2d96628e1f45a9827cd2d3cbe41af835f6d2c19b0"
+  url "https://github.com/michaKFromParis/sparks/releases/download/v0.0.42/sparks-osx.tar.gz"
+  version "0.0.42"
+  sha256 "1d8119ff16505d794f5c33e21ef31240f9578dd95a6e2538dfcb5444d4e42401"
 
   def install
     bin.install "sparks"
